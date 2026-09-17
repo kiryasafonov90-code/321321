087a044f0fde769af4b8e9c68a3c2d0a0778acb1
Safonov <kirya.safonov.90@gmail.com>
2026-09-17 12:38:45 +0500

    Исправления: README, Report, Summary, docker-compose порт 3307, Dockerfile node app.js, удалён мусор

--- conftest.py
+++ conftest.py
@@ -23,7 +23,7 @@ def driver():
 def db_connection():
     conn = mysql.connector.connect(
         host="localhost",
-        port=3306,
+        port=3307,
         user="app",
         password="pass",
         database="app"
